be3967fcd7933f3078f960b3b64367e7a99f644e
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-05-28 23:34:37 -0400

    Added id,username,email and password column

--- models/User.js
+++ models/User.js
@@ -9,3 +9,48 @@ class User extends Model {
       return bcrypt.compareSync(loginPw, this.password);
     }
   }
+
+  // create fields/columns for User model
+User.init(
+    {
+        //define id column
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      //define username column
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+
+      //define email column
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      //define password
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6]
+        }
+      }
+    }
+
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+  
+  module.exports = User;
